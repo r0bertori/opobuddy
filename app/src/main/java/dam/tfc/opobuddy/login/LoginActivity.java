07323a695626edffbdc1ca7f415f0e1bf45e40b4
coral <coralf2004@gmail.com>
2025-04-09 09:53:26 +0200

    Parte visual del Login

---
+++ app/src/main/java/dam/tfc/opobuddy/login/LoginActivity.java
@@ -0,0 +1,17 @@
+package dam.tfc.opobuddy.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import dam.tfc.opobuddy.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
